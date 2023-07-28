ccc762da62dd161cea99112895d130d2583cb7ec
Sre-A <96755517+Sre-A@users.noreply.github.com>
2023-07-28 10:22:33 -0400

    palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -29,11 +29,12 @@ class Solution:
     # Write code below to complete prompt
     def isPalindrome(self, s):
             #type s: string
-            #return type: booleanW
+            #return type: boolean
             
-            #TODO: Write code below to return a boolean value with the solution to the prompt.
-        return (s == s[::-1] and len(s) > 6)
-         
+            #TODO: Write code below to return a boolean value with the solution to the prompt
+            if len(s) > 6 and s == s[::-1]:
+                 return True
+            return False
 
 def main():
     tc1 = Solution()
